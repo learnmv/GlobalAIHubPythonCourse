26dd99b5c454028122f4a5444a1d66de9c0f3777
Mahesh Varma <maheshvarma.dommaraju@gmail.com>
2021-02-02 18:49:12 +0530

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,5 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+#1. Create a list and swap the second half of the list with the first half the list and print this list on the screen
+#2. Ask the user to input a single digit integer to a variable 'n'. Then print out all of the even numbers from 0 to n.
