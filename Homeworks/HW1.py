094493502ed90874c13127a7c0282c8788aa7f1a
learnmv <maheshvarma.dommaraju@gmail.com>
2021-02-02 21:50:43 +0530

    Answers to Day2 Homework

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -2,13 +2,15 @@
 
 #Question 1
 #1. Create a list and swap the second half of the list with the first half the list and print this list on the screen
-#2. Ask the user to input a single digit integer to a variable 'n'. Then print out all of the even numbers from 0 to n.
+
 my_list = [1,2,3,4,5,6]
+
 mid = len(my_list) // 2
-#first half of the list
-first_half = my_list[0:mid]
-#second half of the list
-second_half = my_list[mid:]
-#swapped list
-swapped_list = second_half + first_half
+
+first_half = my_list[0:mid] #first half of the list
+
+second_half = my_list[mid:] #second half of the list
+
+swapped_list = second_half + first_half #swapped list
+
 print(swapped_list) #printing the list on the screen
--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,9 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+#2. Ask the user to input a single digit integer to a variable 'n'. 
+# Then print out all of the even numbers from 0 to n (including n).
+
+n = int(input("Please enter a integer between 0 to 9: ")) #recieving input from the user
+even_numbers = [i for i in range(1,n+1) if i%2 == 0] #list containing even numbers
+print(even_numbers)
