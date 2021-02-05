eedac5bc09fb3855f4a69a83bd1429ca671d7153
learnmv <maheshvarma.dommaraju@gmail.com>
2021-02-05 21:03:34 +0530

    Day2 Homework

---
+++ Homeworks/Day3_1.py
@@ -0,0 +1,17 @@
+#Explain your work
+
+#Question 1
+# User login application:
+# 		-Get Username and Password values from the user
+# 		-Check the values in an if statement and tell the user if they were successful.
+#Extra:
+# -Try Building the same user login application but this time, use a dictionary.
+
+Username = 'JohnLegend'
+Password = 'Iamlegend'
+Input_user = input("Please Enter your username: ")
+Input_pass = input("Please Enter password to proceed: ")
+if (Username == Input_user) and (Password == Input_pass):
+	print("Login is Successful")
+else:
+	print("Enter correct username and password")
---
+++ Homeworks/Day3_2.py
@@ -0,0 +1,13 @@
+#Question 1
+# User login application:
+# 		-Get Username and Password values from the user
+# 		-Check the values in an if statement and tell the user if they were successful.
+#Extra:
+# -Try Building the same user login application but this time, use a dictionary.
+
+user_dict = {'Username':'JohnLegend','Password':'Iamlegend'}
+input_dict = {'Username':input('Please enter your username: '),'Password':input('Plese enter your password to proceed: ')}
+if (user_dict['Username']==input_dict['Username']) and (user_dict['Password'] == input_dict['Password']):
+    print("Login Successful !!!")
+else:
+    print("Please enter correct Username and Password")
